35b47a56d1392f409d54f4c899428c08cf087b2d
DJS3M3N <44145516+DJS3M3N@users.noreply.github.com>
2024-06-06 12:57:50 +0400

    Update start.command.ts

--- src/commands/start.command.ts
+++ src/commands/start.command.ts
@@ -1,21 +1,21 @@
-import {Telegraf } from "telegraf";
+import { Telegraf } from "telegraf";
 import { Command } from "./command.class";
 import { IBotContext } from "../context/context.interface";
 
 export class StartCommand extends Command {
-    constructor(bot: Telegraf<IBotContext>) {
-        super(bot);
-    }
+	constructor(bot: Telegraf<IBotContext>) {
+		super(bot);
+	}
 
-    handle(): void {
-        this.bot.command("start", async ctx => {  
-            const deepLinkPayload = ctx.message.text.substring(6).trim();
-            if (deepLinkPayload) {
-                ctx.session.deepLinkPayload = deepLinkPayload
-                ctx.scene.enter("clan_invite")
-            } else {
-                ctx.scene.enter("greeting")
-            }     
-        });
-    }
+	handle(): void {
+		this.bot.command("start", async (ctx) => {
+			const deepLinkPayload = ctx.message.text.substring(6).trim();
+			if (deepLinkPayload) {
+				ctx.session.deepLinkPayload = deepLinkPayload;
+				ctx.scene.enter("clan_invite");
+			} else {
+				ctx.scene.enter("greeting");
+			}
+		});
+	}
 }
